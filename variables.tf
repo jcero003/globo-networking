05b1392c3a6a51e0369c4c7c691b00fd7f359a15
Jorge C <jcero003@gmail.com>
2026-05-23 23:51:52 -0400

    Add Third subnet and tags

--- variables.tf
+++ variables.tf
@@ -20,6 +20,11 @@ variable "environment" {
   default     = "JCdev"
 }
 
+variable "billing_code" {
+  type        = string
+  description = "Required. Billing code to use for all resources in this module."
+}
+
 variable "cidr_block" {
   type        = string
   description = "(Optional) The CIDR block for the VPC. Default:10.42.0.0/16"
